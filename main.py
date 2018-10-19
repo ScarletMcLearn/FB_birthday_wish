8a6000f1c4c412025f9c83c44a425ea38c545e5d
Akib Sadmanee <akibsadmanee@gmail.com>
2018-10-20 00:48:06 +0600

    DONE

--- main.py
+++ main.py
@@ -10,19 +10,24 @@ else:
 
 print("While entering password nothing may appear on the screen, but be sure to type it right as this option is just for security. ")
 
-#user = input("Enter Your Facebook Login Username, Phone Number or Email:  ")
-#password = getpass.getpass(prompt='Enter Your Facebook Login Password:  ') 
+user = input("Enter Your Facebook Login Username, Phone Number or Email:  ")
+password = getpass.getpass(prompt='Enter Your Facebook Login Password:  ') 
 wish="Wish You Many Many Happy Returns of The Day :D"
-user = "Sadmanee"
-password = "AkIb7711590"
+
 
 driver=webdriver.Chrome(chrome_path)
+
 driver.get("https://www.facebook.com/login")
 driver.find_element_by_id('email').send_keys(user)
 driver.find_element_by_id('pass').send_keys(password)
 driver.find_element_by_id('loginbutton').click()
-driver.get("https://www.facebook.com/")
-respond = driver.execute_script("return document.documentElement.outerHTML")
 
-soup = BeautifulSoup(respond, 'lxml')
-post = soup.select('.rc .r a')
+driver.get("https://www.facebook.com/events/birthdays/")
+areas = driver.find_elements_by_tag_name('textarea')
+
+for area in areas:
+    area.send_keys(wish)
+    area.submit()
+
+driver.quit()
+
